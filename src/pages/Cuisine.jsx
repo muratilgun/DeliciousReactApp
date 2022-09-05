ee32fefe92982f399f5c9c84198a1a94d20f7dd0
muratilgun <muratilgun34@gmail.com>
2022-09-05 19:38:45 +0300

    useParams

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+import { Link, useParams } from 'react-router-dom';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function Cuisine() {
-  return (
-    <div>Cuisine</div>
-  )
+  const [cuisine, setCuisine] = useState([]);
+  let params = useParams();
+  const getCuisine = async (name) => {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`
+    );
+    const recipes = await data.json();
+    setCuisine(recipes.results);
+  };
+  useEffect(() => {
+    getCuisine(params.type);
+    // console.log(params.type);
+  }, [params.type]);
+  return <div></div>;
 }
 
-export default Cuisine
+export default Cuisine;
