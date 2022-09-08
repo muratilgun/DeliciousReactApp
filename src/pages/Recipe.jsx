49036350fd92d97f3ac5a4b6e1cdbc4e6db624db
muratilgun <muratilgun34@gmail.com>
2022-09-09 00:52:08 +0300

    import Link for details page

---
+++ src/pages/Recipe.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+function Recipe() {
+  return <div>Recipe</div>;
+}
+
+export default Recipe;
