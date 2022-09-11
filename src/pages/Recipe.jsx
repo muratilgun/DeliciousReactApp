dd3a9ab5ef02fbbf1534bfcdcd68328d2704a349
muratilgun <muratilgun34@gmail.com>
2022-09-11 19:02:51 +0300

    DetailWrapper

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -18,7 +18,50 @@ function Recipe() {
     fetchDetails();
   }, [params.name]);
 
-  return <div>{details.title}</div>;
+  return (
+    <DetailWrapper>
+      <div>
+        <h2>{details.title}</h2>
+        <img src={details.image} alt="" />
+      </div>
+      <Info>
+        <Button>Instructions</Button>
+        <Button>Ingredients</Button>
+      </Info>
+    </DetailWrapper>
+  );
 }
+
+const DetailWrapper = styled.div`
+  margin-top: 10rem;
+  margin-bottom: 5rem;
+  display: flex;
+  .active {
+    background: linear-gradient(35deg, #494949, #313131);
+    color: white;
+  }
+  h2 {
+    margin-bottom: 2rem;
+  }
+  li {
+    font-size: 1.2rem;
+    line-height: 2.5rem;
+  }
+  ul {
+    margin-top: 2rem;
+  }
+`;
+
+const Button = styled.button`
+  padding: 1rem 2rem;
+  color: #313131;
+  background: white;
+  border: 2px solid black;
+  margin-right: 2rem;
+  font-weight: 600;
+`;
+const Info = styled.div`
+  margin-left: 10rem;
+`;
 
 export default Recipe;
